c2b153295e07974616eb8e1b85126bb49abe6d99
darwinbalino <darwinbalino@gmail.com>
2025-12-30 03:27:20 -0800

    Initial Commit

---
+++ defy-blog-ios/defy_blog_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  defy_blog_iosApp.swift
+//  defy-blog-ios
+//
+//  Created by Darwin Balino on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct defy_blog_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
